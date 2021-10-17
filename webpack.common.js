49ae44ef341b465556484f32821a4aacf8dd5f87
Adam KILUK <adam.kiluk@axadirect-solutions.pl>
2021-10-17 21:02:12 +0200

    Step_08

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,7 @@
-const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: "./src/index.js",
-    mode: "development", 
-    devtool: "inline-nosources-source-map",
-    output: {
-        filename: "main.[contenthash].js",
-        path: path.resolve(__dirname, "dist")
-    },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/template.html"
